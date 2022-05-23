67eca368d2b21aee4b2bda33e1632d34504d4f1c
Your Name <you@example.com>
2022-05-23 21:55:55 +0800

    feat: prepare to use less & less-loader

---
+++ src/component/card/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.less';
+
+const Card = () => {
+    return (
+        <div className={styles.container}>123</div>
+    )
+};
+
+export default Card;
